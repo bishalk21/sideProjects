b6ccf90a36ff998097274ef728a8bb475965d733
Your Name <karkibishal00@gmail.com>
2022-08-15 09:01:39 +1000

    add two numbers pss

--- problem-solving/problemSolving.js
+++ problem-solving/problemSolving.js
@@ -1,4 +1,4 @@
-// Two Sum
+// Day 1: Two Sum
 // https://leetcode.com/problems/two-sum/
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -54,3 +54,61 @@ function twoSum(nums, target) {
         map[nums[i]] = i;
     }
 }
+
+//  Day 2: Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+// Solution:
+// 1. Create a new linked list
+// 2. Loop through the two linked lists and add the values
+// 3. If the sum is greater than 10, add the remainder to the next node
+// 4. If the sum is less than 10, add the sum to the next node
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+function addTwoNumbers(l1, l2) {
+    let newList = new ListNode(0); // create a new list
+    let current = newList; // set the current node to the new list
+    let carry = 0; // set the carry to 0 initially
+    console.log(l1, l2);
+    while (l1 || l2) { // while either list has a value
+        let sum = carry;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+    }
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+    return newList.next;
+}
+
+// Solution 2 with values as keys and indices as values:
+var addTwoNumbers = function (l1, l2) {
+    let carry = 0;
+    let result = new ListNode(0);
+    let current = result;
+    while (l1 || l2 || carry) {
+        let sum = (l1 && l1.val) + (l2 && l2.val) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        l1 = l1 && l1.next;
+        l2 = l2 && l2.next;
+    }
+    return result.next;
+};
+
+// example 1:
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
